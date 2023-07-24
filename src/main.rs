34c2800509c8f6f06be9382cde59dcd2da63b1e4
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2023-07-24 14:50:53 -0500

    Added axum and wrote the first handler.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use axum::Server;
+use axum::Router;
+use axum::routing::get;
+
+async fn greet() -> String {
+    "Hello, That Conf!".to_string()
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, that conf!");
+    let app = Router::new().route("/", get(greet));
+    Server::bind(&"0.0.0.0:3333".parse().unwrap())
+           .serve(app.into_make_service()).await.unwrap();
 }
+// cargo add tokio --features full
+// cargo watch -c -q -x run
